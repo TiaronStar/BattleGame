f9b102164cb58202b44b8a71bad23059af2a13c9
TiaronStar <101217523+TiaronStar@users.noreply.github.com>
2023-03-25 17:02:05 -0700

    Update title.cpp

---
+++ BattleGame/title.cpp
@@ -0,0 +1,25 @@
+/****************************************************************************
+* File:      ascii_title.h
+* Author:    Adam Stanley, Tiaron Starrine
+* Purpose:   Contains title ascii image
+* Version:   1.0 Mar 23, 2023
+* Resources: Used in-class/lab time to complete.
+*******************************************************************************/
+#include <iostream>
+#include<string>
+#include<fstream>
+#include "title.h"
+using namespace std;
+
+void printTitle(void) {
+	cout << "loading game title...\n" << endl;
+	cout << "______                                            __  __              _" << endl;
+	cout << "|  _  |                                          |  |/  |            | |  " << endl;
+	cout << "| | | | _   _  _ __   ___ _   ___   ___   _ __   | .  . |  __ _  ___ | |_   ___  _ __ " << endl;
+	cout << "| | | || | | || '_  || _`  | / _ ||  _  || '_ |  | ||/| | / _` |/ __|| __| / _ || '__|" << endl;
+	cout << "| |/ / | |_| || | | || (_| || |__/| (_) || | | | | |  | || (_| ||__ || |_ | |__/| |   " << endl;
+	cout << "|___/  |__,__||_| |_||__,  ||____||_____||_| |_| |_|  |_/|__,_||___/ |__| |___| |_|   " << endl;
+	cout << "                       __/ |                                                          " << endl;
+	cout << "                      |___/                                                           " << endl;
+	cout << endl;
+}
