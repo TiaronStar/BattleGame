396c23d75ce7497b7aa037a1dd9e54850aefb4b1
TiaronStar <101217523+TiaronStar@users.noreply.github.com>
2023-04-01 13:41:54 -0700

    Create map.h

---
+++ map.h
@@ -0,0 +1,16 @@
+#pragma once
+/****************************************************************************
+* File:      ascii_map.h
+* Author:    Adam Stanley, Tiaron Starrine
+* Purpose:   Contains title ascii image
+* Version:   1.0 Mar 23, 2023
+* Resources: Used in-class/lab time to complete.
+*******************************************************************************/
+#include <iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+void initialMap(void);
+void movement(void);
+void gameMap(void);
